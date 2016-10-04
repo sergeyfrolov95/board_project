f51cc893338c031d7ab9ae126adf79f2f918c2ee
Sergey <sergeyfrolovapp@gmail.com>
2016-10-04 18:40:51 +0300

    Finnaly did forms

--- imgboard/urls.py
+++ imgboard/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'imgboard'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^thread(?P<thread_id>[0-9]+)/$', views.thread, name='thread')
+	url(r'^thread(?P<thread_id>[0-9]+)/$', views.ThreadView.as_view(), name='thread')
 ]
--- imgboard/views.py
+++ imgboard/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import View
+from django.utils import timezone
 
 from .models import Thread, Post
+from .forms import PostForm
 
 def index(request):
 	thread_list = Thread.objects.all()
 	context = {'thread_list': thread_list}
 	return render(request, 'imgboard/index.html', context)
 
-def thread(request, thread_id):
-	thread = get_object_or_404(Thread, pk=thread_id)
-	context = {
-		'thread': thread,
-	}
-	return render(request, 'imgboard/thread.html', context)
+class ThreadView(View):
+	post_form = PostForm
+	initial = {'key': 'value'}
+
+	def get(self, request, thread_id, **keargs):
+		thread = get_object_or_404(Thread, pk=thread_id)
+		form = self.post_form(initial=self.initial)
+		context = {
+			'thread': thread,
+			'form': form,
+		}
+		return render(request, 'imgboard/thread.html', context)
+
+	def post(self, request, **kwargs):
+		form = self.post_form(request.POST)
+		if form.is_valid():
+			p = Post(post_text=form.cleaned_data['post_text'],
+				date_published=timezone.now(), thread_id=kwargs['thread_id'])
+			p.save()
+			return HttpResponseRedirect('')
+		else:
+			return HttpResponseRedirect('')
